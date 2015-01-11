3a656e60a4eeb3557f3dfe92d33f699c0d59d6f6
Adam Brown <adamarthurryan@gmail.com>
2015-01-11 15:24:03 +0000

    add bootstrap styling

--- routes/main.js
+++ routes/main.js
@@ -1,7 +1,7 @@
 var ctrl = require('../app_server/controllers/main');
 
-//route the root path to the main controller's index method
 module.exports = function(app) {
+  //route the root path to the main controller's index method
   app.get('/', ctrl.index); 
 }  
 
